90d02e9d5587581069e6d256270f2b98957a75f1
HaroldDeRidder <r0720137@student.thomasmore.be>
2023-01-09 19:44:44 +0100

    database files

--- app/RecipeV2.py
+++ app/RecipeV2.py
@@ -1,8 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+import random
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import auth
 
+import crud
+import models
+import schemas
+from database import SessionLocal, engine
+import os
+if not os.path.exists('.\sqlitedb'):
+    os.makedirs('.\sqlitedb')
+
+#"sqlite:///./sqlitedb/sqlitedata.db"
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 class Recipe(BaseModel):
     id: int
@@ -132,3 +157,25 @@ def delete_recipe(recipe_id: int):
     recipe = next((r for r in recipes if r.id == recipe_id), None)
     if recipe is None:
         return {"error": "Recipe not found"}
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
